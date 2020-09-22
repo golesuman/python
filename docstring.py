ca18be7b325872e570fe89b4a7681cb662ed6533
Suman Gole <60792767+golesuman@users.noreply.github.com>
2020-09-22 16:44:07 +0545

    Add files via upload

---
+++ docstring.py
@@ -0,0 +1,7 @@
+def mul(a,b):
+	"""This will calculate the sum of two numbers"""
+	m=a*b
+	return m
+i=mul(2,3)	
+print(mul.__doc__)
+	
---
+++ mul.py
@@ -0,0 +1,6 @@
+def mul(a,b):
+	m=a*b
+	return m
+i=mul(2,3)	
+print(i)
+	
