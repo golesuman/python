5deb58cdb5897427a8d7e2c75519a5c52c5d0f0e
Suman Gole <60792767+golesuman@users.noreply.github.com>
2021-07-19 09:14:59 +0545

    Add files via upload

--- osmodule.py
+++ osmodule.py
@@ -9,8 +9,9 @@ os.rename('Psychopath','Psycopath1') # rename the file name
 print(os.listdir())
 os.makedirs('PyschoSuman/SumanGole') #make directories 
 os.removedirs('PyschoSuman/SumanGole') #remove the directories
-for dirpath,dirnames,filenames in os.walk('/media/suman/649EF25A9EF223E8'):
-    print('Current Path:', dirpath)
-    print('Directories:', dirnames)
-    print('Files:', filenames)
-    print()
+os.path.join('/media','OOP')
+print(os.path.join('media','suman'))
+print(os.listdir())
+print(dir(os))
+print(os.getcwd())
+print(os.path.isdir('venv'))
