6475a4610e262a4fbab93c9d0f9b0e0e96cdec8c
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-03-18 13:41:03 +0545

    walrus operator

---
+++ walrusoperator.py
@@ -0,0 +1,27 @@
+# walrus operator :=
+# from http.server import BaseHTTPRequestHandler
+
+
+# print(happy:=True)
+
+
+# foods=[]
+
+# while True:
+
+#     food=input('what food do you like ? :')
+
+#     if food=='quit':
+#         break
+
+#     foods.append(food)
+
+# print(foods)
+
+
+#alternatively
+foods=[]
+
+while food:=input('What food do you like ?:') !='quit':
+    foods.append(food)
+print(foods)
