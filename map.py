a94a38e7d0ca6cef0085cd69555c60bf22a5244f
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-03-18 14:37:38 +0545

    map function

--- map.py
+++ map.py
@@ -1,2 +1,16 @@
-l=[1,2,3,4]
-print(list(map(lambda m:m**3,l)))
+# #map(function,iterable)
+
+
+store=[('shirt',20.00),
+        ('pants',30.00),
+        ('Jackets',50.00),
+        ('socks',10.00)]
+
+
+to_euros=lambda data: (data[0],data[1]*0.82)
+# store_euros=list(map(to_euros,store))
+
+# for i in store_euros:
+#     print(i)
+to_euros(store)
+# print(store[1])
