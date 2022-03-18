8fb3fd2835a64c4bf2f5237f6b32cadacc10d0de
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-03-18 13:45:18 +0545

    assigning the function to variable

---
+++ assignfunctiontovariable.py
@@ -0,0 +1,18 @@
+# def hello():
+#     print('hello')
+
+
+
+# print(hello) #prints the address of the hello variable
+
+
+# hi=hello
+# hi()
+# hello()
+# print(hi)
+
+
+say=print
+
+say("I can't believe this actually works")
+
